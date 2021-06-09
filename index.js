e8ba2303801283138af1a27c8876220edd17acfa
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-06-08 21:32:26 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express()
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/admin', function (req, res) {
+  res.send('you are on admin route')
+})
+
+app.get('/agent', function (req, res) {
+  res.send('you are on agent route')
+})
+
+app.get('/cashier', function (req, res) {
+  res.send('you are on cashier')
+})
+ 
+app.listen( process.env.PORT || 5000);
